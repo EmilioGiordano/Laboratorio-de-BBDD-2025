26a22f87e9ceb9af25dbd1903038697527d37196
EmilioGiordano <giordanoemilio21@gmail.com>
2025-05-28 15:43:39 -0300

    Agrego archivo .sql para los dominios y restricciones, hago correciones sobre el archivo del Drive

---
+++ 1AB_dominios_restricciones_DDL.sql
@@ -0,0 +1,28 @@
+CREATE DOMAIN gramos AS CHAR(4)
+CHECK(
+   VALUE IN ('125','250', '500', '1000')
+);
+
+ALTER TABLE helados ALTER COLUMN peso_gramos TYPE gramos;
+
+CREATE DOMAIN ingredientes AS INTEGER
+CHECK(
+   VALUE IN (0,1,2,3,4)
+);
+
+ALTER TABLE helados ALTER COLUMN cant_ingredientes TYPE ingredientes;
+
+CREATE DOMAIN turnos_planta AS CHAR(6)
+CHECK(
+   VALUE IN ('MAÑANA','TARDE', 'NOCHE')
+);
+
+ALTER TABLE planta ALTER COLUMN turno TYPE turnos_planta;
+
+CREATE DOMAIN turnos_emp AS CHAR(6)
+CHECK(
+   VALUE IN ('MAÑANA','TARDE')
+);
+
+ALTER TABLE administrativos ALTER COLUMN turno TYPE turnos_emp;
+ALTER TABLE repartidores ALTER COLUMN turno TYPE turnos_emp;
